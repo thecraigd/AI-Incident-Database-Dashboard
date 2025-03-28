6721dbf4b61add8b20c5d5827311aedb216c0de9
Craig Dickson <thecraigdickson@gmail.com>
2025-03-28 17:46:25 +0100

    Configure Netlify to skip TypeScript errors during build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_SKIP_TYPE_CHECK === 'true',
+  },
 };
 
 module.exports = nextConfig;
